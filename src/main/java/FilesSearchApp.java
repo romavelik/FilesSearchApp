e42ac13cc4ba54bde248347739a9ba1e5717775f
Roman Velikorod <romavelik@gmail.com>
2020-01-20 02:34:23 +0200

    added to git

---
+++ src/main/java/FilesSearchApp.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FilesSearchApp {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            System.out.println("Enter the rootPath. For example: D:\\\\dir1\\dir2");
+            String rootPath = reader.readLine();
+            System.out.println("Enter the depth value. The depth of root directory is 0");
+            Integer depth = Integer.valueOf(reader.readLine());
+            System.out.println("Enter the mask");
+            String mask = reader.readLine();
+            Util.searchFiles(rootPath, depth, mask);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/Util.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Util {
+
+    public static void searchFiles(String rootPath, Integer depth, String mask){
+        String[] directories = directorySequence(rootPath);
+        int limit = directories.length - 1 - depth;
+        printFoundFiles(rootPath, lookHere(mask, rootPath));
+        for (int i = directories.length-2; i>=limit; i--){
+            String subPath = rootPath.split("\\\\".concat(directories[i+1]))[0];
+            printFoundFiles(subPath, lookHere(mask, subPath));
+        }
+    }
+
+    private static String[] directorySequence(String rootPath){
+        return rootPath.split("\\\\");
+    }
+
+    private static void printFoundFiles(String directory, List<String> fileNames){
+        System.out.printf("Files in %s\n", directory);
+        if(fileNames.size()==0){
+            System.out.println("No files matching the mask");
+        }
+        for (String fileName : fileNames){
+            System.out.printf("\t| %s\n", fileName);
+        }
+    }
+
+    private static List<String> lookHere(String mask, String path) {
+        Pattern pattern = Pattern.compile("(.*)".concat(mask).concat("(.*)"));
+        File directory = new File(path);
+        File[] filesArray = directory.listFiles();
+        List<String> files;
+        if (filesArray == null) {
+            files = Arrays.asList("Directory is absolutely empty or doesn't exist at all");
+        } else {
+            files = Arrays.stream(filesArray).map(file -> file.getName()).filter(String->pattern.matcher(String).matches()).collect(Collectors.toList());
+        }
+        return files;
+    }
+}
